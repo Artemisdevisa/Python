1a925f4b565637103710934322ad4379df8b3cdc
artemis <artemisdevisa@gmail.com>
2024-07-16 21:40:52 -0500

    updates

--- Dia_10/main.py
+++ Dia_10/main.py
@@ -3,18 +3,39 @@ import pygame
 # Inicializar a pygame
 pygame.init()
 
-# Crear la pantalla con un ancho y un largo definido
+# Crear la pantalla con un ancho y un largo definido / ancho y largo
 pantalla = pygame.display.set_mode((800, 600))
 
 # Título e ícono
-pygame.display.set_caption("Invasion Espacial", )
+pygame.display.set_caption("Artemis")
+icono = pygame.image.load("64x64.ico")
+pygame.display.set_icon(icono)
 
-# Crear un loop hasta que mi evento de cierre se ejecute
+# Jugador
+img_jugdor = pygame.image.load("nave.png")
+jugador_x = 368
+jugador_y = 536
+
+def jugador(x, y):
+    pantalla.blit(img_jugdor, (x, y))
+
+# Crear un loop del juego
 se_ejecuta = True
 while se_ejecuta:
+    # Asignar fondo RGB al juego
+    pantalla.fill((205, 104, 228))
+
+    jugador_x -= 0.1
+
     for evento in pygame.event.get():
         # evento tipo QUIT - hacer click en la x
         if evento.type == pygame.QUIT:
             se_ejecuta = False
+
+    jugador(jugador_x, jugador_y)
+
+    # Actualizar pantalla para que se muestre el fondo
+    pygame.display.update()
+
 
 
--- Dia_9/Proyecto_Dia9.py
+++ Dia_9/Proyecto_Dia9.py
@@ -1,26 +1,32 @@
 import os
+import re
+import datetime
+
 grande = ""
 cadenita = []
 ruta = "C:\\Users\\jenny\\Desktop\\Mi_Gran_Directorio"
 '''------------------------------------------------------------------------------------------'''
-os.chdir(f"C:\\Users\\jenny\\Desktop\\Mi_Gran_Directorio")
+
 def lectura(ruta):
     for carpeta, subcarpeta, archivos in os.walk(ruta):
-        print(carpeta)
-        for sub in subcarpeta:
-            print(sub)
         for arch in archivos:
-                print(arch)
-
+            abrir = open(carpeta + "\\" + arch, 'r')
+            salida = abrir.read()
+            resultado = re.search(r'N\w{3}-\d{5}', salida)
+            if resultado:
+                cadenita.append(arch+"-"+resultado.group())
 
 lectura(ruta)
 
 def formato():
-    grande = ("ARCHIVO\t\t\t\tNRO.SERIE\n"
+    grande = f'Fecha de búsqueda:\t\t\t\t[{datetime.date.today()}]\n'
+    grande += ("ARCHIVO\t\t\t\tNRO.SERIE\n"
               "-------\t\t\t\t----------\n")
     for val in cadenita:
         grande += f"{val.split("-")[0]}\t\t\t\t{val.split("-")[1]}\n"
-
+    grande += f'Números encontrados: {len(cadenita)}'
     return grande
+
+print(formato())
 
 
